59fe4dffb026c0b685a23b346aa99ece74137156
vipertecpro <vipertecpro@gmail.com>
2019-07-04 09:06:52 +0530

    Initial commit

---
+++ resources/views/conversationList.blade.php
@@ -0,0 +1,3 @@
+<div class="text-left">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid autem consequatur delectus dignissimos doloribus hic id laborum minima odit perspiciatis, quas quibusdam quos rerum similique totam veritatis. Aliquam, nostrum?</p>
+</div>
